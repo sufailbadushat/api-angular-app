f85c05f3ebeafdacc6fb7a4028509391ab56450c
sufailbadushat <sufailbadushat@gamil.com>
2022-11-29 17:04:27 +0530

    us public dynamic data add

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,8 @@ export class ApiService {
   fetchPassnger=()=>{
     return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")
   }
+
+  fetchUsPublic=()=>{
+    return this.http.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+  }
 }
